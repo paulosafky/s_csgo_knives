043bd74a475dc98e21f1da572b303d097e42308b
SAFKY <62437089+paulosafky@users.noreply.github.com>
2024-06-13 10:31:12 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,14 +1,18 @@
 author "safky"
-fx_version 'cerulean'
+fx_version "cerulean"
 game "gta5"
 version "1.0.0"
 description 'CS GO Knives by SAFKY DEV'
+
+files {
+    'metas/**/*.meta'
+}
 
 data_file 'WEAPON_METADATA_FILE' 'metas/**/weaponarchetypes.meta'
 data_file 'WEAPON_ANIMATIONS_FILE' 'metas/**/weaponanimations.meta'
 data_file 'PED_PERSONALITY_FILE' 'metas/**/pedpersonality.meta'
 data_file 'WEAPONINFO_FILE' 'metas/**/weapons.meta'
-
 client_script 'cl_weaponNames.lua'
+
 
 lua54 'yes'
